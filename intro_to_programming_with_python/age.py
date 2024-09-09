b6f887d27c2067e8a8885d772855582597161f90
Leah M. Fulmer <leahmfulmer@Jai-Ma-Kali.local>
2024-09-09 16:25:13 -0500

    submitting variables exercises

---
+++ intro_to_programming_with_python/age.py
@@ -0,0 +1,6 @@
+age = 29
+
+print(f"You are {age} years old.")
+
+for i in range(1,5):
+    print(f"In {10 * i} years, you will be {age + (10 * i)} years old.")
---
+++ intro_to_programming_with_python/greeter.py
@@ -0,0 +1,5 @@
+name = "Victor"
+
+print(f'Good Morning, {name}.')
+print(f'Good Afternoon, {name}.')
+print(f'Good Evening, {name}.')
