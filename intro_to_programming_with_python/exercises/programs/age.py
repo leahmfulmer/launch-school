19c6a3ea01327f46c306a7ac8cf836bf7af7d0f1
Leah M. Fulmer <leahmfulmer@gmail.com>
2024-09-11 15:43:29 -0500

    reorganizing directories

--- intro_to_programming_with_python/exercises/programs/age.py
+++
@@ -1,6 +0,0 @@
-age = int(input("How old are you? "))
-
-print(f"You are {age} years old.")
-
-for i in range(1,5):
-    print(f"In {10 * i} years, you will be {age + (10 * i)} years old.")
--- intro_to_programming_with_python/exercises/programs/greeter.py
+++
@@ -1,4 +0,0 @@
-first = input("What is your first name? ")
-last = input("What is your last name? ")
-
-print(f'Hello, {first} {last}!')
