c3d376bbba2133e6694531d7b816453a6f6f84ee
Leah M. Fulmer <leahmfulmer@gmail.com>
2024-10-14 17:09:58 -0500

    submitting exercises for Inheritance

---
+++ books/object_oriented_programming/exercises/signal_mixin.py
@@ -0,0 +1,10 @@
+class SignalMixin:
+    
+    def signal_left(self):
+        print('Signalling left')
+
+    def signal_right(self):
+        print('Signalling right')
+
+    def signal_off(self):
+        print('Signal is now off')
