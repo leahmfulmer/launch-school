a986c4d8565a52e5bf4458a747b54f0fda9cf0f1
Leah M. Fulmer <leahmfulmer@gmail.com>
2025-02-03 11:49:55 -0600

    adding dict_reassignment.py

---
+++ py110/lesson_1/dict_reassignment.py
@@ -0,0 +1,13 @@
+dict1 = {
+    'apple': 'Produce',
+    'carrot': 'Produce',
+    'pear': 'Produce',
+    'broccoli':  'Produce',
+}
+
+dict1['apple'] = 'Fruit'
+dict1['carrot'] = 'Vegetable'
+dict1['pear'] = 'Fruit'
+dict1['broccoli'] = 'Vegetable'
+
+print(dict1)
