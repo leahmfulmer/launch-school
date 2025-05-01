a62fadb422000e690d2843d8baf4a347d22adfe8
Leah M. Fulmer <leahmfulmer@gmail.com>
2025-05-01 18:55:57 -0500

    updating pedac_most_adjacent_consonants.py

--- py110/lesson_1/pedac_most_adjacent_consonants.py
+++ py110/lesson_1/pedac_most_adjacent_consonants.py
@@ -1,9 +1,9 @@
 # 1. For each string in the input list, determine the highest number of adjacent consonants within that string.
 
-#### a. Initialize a helper function `is_vowel` which takes an input string `letter` as its parameter and returns a boolean object that describes whether or not that letter is a vowel.
+#### a. Initialize a helper function `is_consonant` which takes an input string `letter` as its parameter and returns a boolean object that describes whether or not that letter is a consonant.
 
 def is_consonant(letter):
-    return (letter not in ('a', 'e', 'i', 'o', 'u'))
+    return (letter not in 'aeiou')
 
 #### b. Initialize a function `sort_by_consonant_count` which takes a list `my_list` as a parameter.
 
@@ -11,11 +11,11 @@ def sort_by_consonant_count(my_list):
     consonant_count = {}
 
     for i, string in enumerate(my_list):
-        string = string.replace(' ', '')
+        string_no_spaces = string.replace(' ', '')
         count = 0
  
-        for j, letter in enumerate(string[:-1]):
-            if is_consonant(string[j]) and is_consonant(string[j+1]):
+        for j, letter in enumerate(string_no_spaces[:-1]):
+            if is_consonant(string_no_spaces[j]) and is_consonant(string_no_spaces[j+1]):
                 count += 1
             else:
                 pass
@@ -33,25 +33,25 @@ def sort_by_consonant_count(my_list):
 
 # Example 1
 my_list = ['aa', 'baa', 'ccaa', 'dddaa'] 
-print(sort_by_consonant_count(my_list)) 
-# ['dddaa', 'ccaa', 'aa', 'baa']
+print(sort_by_consonant_count(my_list) == ['dddaa', 'ccaa', 'aa', 'baa']) 
+# True
 
 # Example 2
 my_list = ['can can', 'toucan', 'batman', 'salt pan'] 
-print(sort_by_consonant_count(my_list))
-# ['salt pan', 'can can', 'batman', 'toucan']
+print(sort_by_consonant_count(my_list) == ['salt pan', 'can can', 'batman', 'toucan'])
+# True
 
 # Example 3
 my_list = ['bar', 'car', 'far', 'jar'] 
-print(sort_by_consonant_count(my_list)) 
-# ['bar', 'car', 'far', 'jar']
+print(sort_by_consonant_count(my_list) == ['bar', 'car', 'far', 'jar']) 
+# True
 
 # Example 4
 my_list = ['day', 'week', 'month', 'year'] 
-print(sort_by_consonant_count(my_list))
-# ['month', 'day', 'week', 'year']
+print(sort_by_consonant_count(my_list) == ['month', 'day', 'week', 'year'])
+# True
 
 # Example 5
 my_list = ['xxxa', 'xxxx', 'xxxb'] 
-print(sort_by_consonant_count(my_list)) 
-# ['xxxx', 'xxxb', 'xxxa']
+print(sort_by_consonant_count(my_list) == ['xxxx', 'xxxb', 'xxxa']) 
+# True
